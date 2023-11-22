363a79096649761c56d37a43eada607b0e236c36
jitpatelonline24 <jitpatelonline24@gmail.com>
2023-11-22 22:57:36 +0000

    sw-2 project

--- src/browserfactory/BaseTest.java
+++ src/browserfactory/BaseTest.java
@@ -1,2 +1,62 @@
-package browserfactory;public class BaseTest {
+package browserfactory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.time.Duration;
+
+/**
+ * BaseUrl = https://www.saucedemo.com/
+ * 1. Create the package ‘browserfactory’ and create the
+ * class with the name ‘BaseTest’ inside the
+ * ‘browserfactory’ package. And write the browser setup
+ * code inside the class ‘Base Test’.
+ * 2. Create the package ‘testsuite’ and create the
+ * following class inside the ‘testsuite’ package.
+ * 1. LoginTest
+ * 3. Write down the following test into ‘LoginTest’ class
+ * 1. userSholdLoginSuccessfullyWithValid Credentials
+ * * Enter “standard_user” username
+ * * Enter “secret_sauce” password
+ * * Click on ‘LOGIN’ button
+ * * Verify the text “PRODUCTS”
+ * 2. verifyThatSixProductsAreDisplayedOnPage
+ * * Enter “standard_user” username
+ * * Enter “secret_sauce” password
+ * * Click on ‘LOGIN’ button
+ * * Verify that six products are displayed on
+ * page
+ */
+public class BaseTest {
+
+    static String browser = "Chrome";
+    public static WebDriver driver;
+
+    public void openBrowser(String baseUrl) {
+        if (browser.equalsIgnoreCase("Chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disable-notifications");
+            options.addArguments("--incognito");
+            driver = new ChromeDriver(options);
+        } else if (browser.equalsIgnoreCase("Firefox")) {
+            driver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("Edge")) {
+            driver = new EdgeDriver();
+        } else if (browser.equalsIgnoreCase("Safari")) {
+            driver = new SafariDriver();
+        } else {
+            System.out.println("Wrong Browser Name");
+        }
+        driver.get(baseUrl);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    public void closedBrowser() {
+        driver.quit();
+    }
 }
--- src/testsuite/LoginTest.java
+++ src/testsuite/LoginTest.java
@@ -1,2 +1,65 @@
-package testsuite;public class LoginTest {
+package testsuite;
+
+import browserfactory.BaseTest;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class LoginTest extends BaseTest {
+
+    static String baseUrl = "https://courses.ultimateqa.com/";
+
+    @Before
+    public void openBrowser() {
+        openBrowser(baseUrl);
+    }
+
+    @Test
+    public void userShouldNavigateToLoginPageSuccessfully() throws InterruptedException {
+        //Find the Sign link on home page and click on it.
+        WebElement signLink = driver.findElement(By.xpath("//a[contains(text(),'Sign In')]"));
+        signLink.click();
+
+        String expectedMessage = "Welcome Back!";
+
+        WebElement displayedMessage = driver.findElement(By.xpath("//h2[contains(text(),'Welcome Back!')]"));
+        String actualMessage =displayedMessage.getText();
+
+        Assert.assertEquals(expectedMessage, actualMessage);
+        Thread.sleep(5000);
+    }
+    @Test
+    public void verifyTheErrorMessage() throws InterruptedException {
+
+
+        WebElement signLink1 = driver.findElement(By.partialLinkText("Sign"));
+        signLink1.click();
+
+        WebElement emailField = driver.findElement(By.id("user[email]"));
+        emailField.sendKeys("jit123@gmail.com");
+        WebElement passField = driver.findElement(By.name("user[password]"));
+        passField.sendKeys("Pass12");
+
+        WebElement loginLink1 = driver.findElement(By.xpath("(//button[@type='submit'])"));
+        loginLink1.click();
+        Thread.sleep(10000);
+        Thread.sleep(10000);
+
+        String expectedMessage = "Invalid email or password.";
+
+        WebElement actualText = driver.findElement(By.xpath("//li[@class='form-error__list-item']"));
+
+        String actualMessage = actualText.getText();
+
+        Assert.assertEquals(expectedMessage,actualMessage);
+    }
+
+
+    @After
+    public void tearDown(){
+        closedBrowser();
+    }
 }
